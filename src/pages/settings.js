4f631900048e435699e54980453776c33e0c1986
shangsuru <helm.henry99@gmail.com>
2023-06-14 12:13:44 +0900

    Fetch and update user data in settings

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,25 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CheckOutlined } from '@ant-design/icons';
 import { Button, Input, Select, Row, Col } from 'antd';
 const { Option } = Select;
 const { TextArea } = Input;
-import { Amplify } from 'aws-amplify';
+import { Amplify, API } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import config from '@/aws-exports';
 import MenuBar from '@/components/MenuBar';
+import { getUser } from '@/graphql/queries';
+import { updateUser } from '@/graphql/mutations';
+import { countries } from '@/utils/countries';
 
 Amplify.configure({ ...config, ssr: true });
 
 function Settings({ user, signOut }) {
-	const [country, setCountry] = useState('Germany');
-	const [location, setLocation] = useState('');
+	const [country, setCountry] = useState('');
+	const [city, setCity] = useState('');
 	const [biography, setBiography] = useState('');
-	const [givenName, setGivenName] = useState('');
-	const [surName, setSurName] = useState('');
+	const [firstname, setFirstname] = useState('');
+	const [lastname, setLastname] = useState('');
 
-	function handleSubmit() {
-		// Update profile information
-		return null;
+	useEffect(() => {
+		API.graphql({
+			query: getUser,
+		}).then(({ data }) => {
+			const { getUser: user } = data;
+			setCountry(user.country);
+			setCity(user.city);
+			setBiography(user.biography);
+			setFirstname(user.firstname);
+			setLastname(user.lastname);
+			console.log(user);
+		});
+	}, []);
+
+	async function handleSubmit() {
+		const res = await API.graphql({
+			query: updateUser,
+			variables: {
+				input: {
+					id: user.attributes.sub,
+					firstname: firstname,
+					lastname: lastname,
+					biography: biography,
+					country: country,
+					city: city,
+				},
+			},
+		});
 	}
 
 	function handleKeyPress(e) {
@@ -39,18 +67,17 @@ function Settings({ user, signOut }) {
 				<Row gutter={[40, 40]}>
 					<Col span={12}>
 						<div className='edit__label'>Country</div>
-						<Select defaultValue={country} onChange={(value) => setCountry(value)} style={{ width: '50%' }}>
-							<Option value='Germany'>Germany</Option>
-							<Option value='USA'>USA</Option>
-							<Option value='Korea'>Korea</Option>
-							<Option value='Taiwan'>Taiwan</Option>
-							<Option value='Sweden'>Sweden</Option>
-							<Option value='France'>France</Option>
+						<Select value={country} onChange={(value) => setCountry(value)} style={{ width: '50%' }}>
+							{countries.map((country) => (
+								<Option key={country} value={country}>
+									{country}
+								</Option>
+							))}
 						</Select>
 					</Col>
 					<Col span={12}>
-						<div className='edit__label'>Location</div>
-						<Input value={location} onChange={(e) => setLocation(e.target.value)} onKeyPress={(e) => handleKeyPress(e)} />
+						<div className='edit__label'>City</div>
+						<Input value={city} onChange={(e) => setCity(e.target.value)} onKeyUp={(e) => handleKeyPress(e)} />
 					</Col>
 				</Row>
 				<Row gutter={[40, 40]}>
@@ -60,18 +87,18 @@ function Settings({ user, signOut }) {
 							value={biography}
 							onChange={(e) => setBiography(e.target.value)}
 							rows={4}
-							onKeyPress={(e) => handleKeyPress(e)}
+							onKeyUp={(e) => handleKeyPress(e)}
 						/>
 					</Col>
 				</Row>
 				<Row gutter={[40, 40]}>
 					<Col span={12}>
-						<div className='edit__label'>Given Name</div>
-						<Input value={givenName} onChange={(e) => setGivenName(e.target.value)} onKeyPress={(e) => handleKeyPress(e)} />
+						<div className='edit__label'>First Name</div>
+						<Input value={firstname} onChange={(e) => setFirstname(e.target.value)} onKeyUp={(e) => handleKeyPress(e)} />
 					</Col>
 					<Col span={12}>
-						<div className='edit__label'>Last name</div>
-						<Input value={surName} onChange={(e) => setSurName(e.target.value)} onKeyPress={(e) => handleKeyPress(e)} />
+						<div className='edit__label'>Last Name</div>
+						<Input value={lastname} onChange={(e) => setLastname(e.target.value)} onKeyUp={(e) => handleKeyPress(e)} />
 					</Col>
 				</Row>
 				<Row justify='end'>
