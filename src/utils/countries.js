958d73fa967aef10b51c41ee11aaa53b7f53bad2
shangsuru <helm.henry99@gmail.com>
2023-06-14 12:13:14 +0900

    Extract flag generation into utils file

---
+++ src/utils/countries.js
@@ -0,0 +1,54 @@
+const countries = ['Germany', 'USA', 'Korea', 'China', 'Japan', 'Sweden', 'France', 'Russia', 'India'];
+
+function renderFlag(country) {
+	let altText, countryID;
+	switch (country) {
+		case 'Korea':
+			altText = 'Korea';
+			countryID = 'KR';
+			break;
+		case 'China':
+			altText = 'China';
+			countryID = 'CN';
+			break;
+		case 'Sweden':
+			altText = 'Sweden';
+			countryID = 'SE';
+			break;
+		case 'France':
+			altText = 'France';
+			countryID = 'FR';
+			break;
+		case 'USA':
+			altText = 'USA';
+			countryID = 'US';
+			break;
+		case 'Germany':
+			altText = 'Germany';
+			countryID = 'DE';
+			break;
+		case 'Russia':
+			altText = 'Russia';
+			countryID = 'RU';
+			break;
+		case 'India':
+			altText = 'India';
+			countryID = 'IN';
+			break;
+		default:
+			altText = 'Japan';
+			countryID = 'JP';
+			break;
+	}
+
+	return (
+		<img
+			width='5%'
+			alt={altText}
+			src={`http://catamphetamine.gitlab.io/country-flag-icons/3x2/${countryID}.svg`}
+			style={{ marginRight: '7px', transform: 'translate(0, -0.2vw)' }}
+		/>
+	);
+}
+
+export { countries, renderFlag };
